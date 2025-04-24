249618a03aa50b2b4f79efcd18d4dc0be34563df
GregoBHM <grehuanca@upt.pe>
2025-04-23 22:59:19 -0500

    Region Change

--- infra/main.tf
+++ infra/main.tf
@@ -37,7 +37,7 @@ resource "random_integer" "ri" {
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
   name     = "upt-arg-${random_integer.ri.result}"
-  location = "westeurope"
+  location = "southeastasia"
 }
 
 # Create the Linux App Service Plan
