804cadbefe2f2d22f8fe9645fc633f3cd522dc59
GregoBHM <grehuanca@upt.pe>
2025-04-24 19:46:52 -0500

    Version 1.0 - deploy

--- infra/main.tf
+++ infra/main.tf
@@ -46,7 +46,7 @@ resource "azurerm_service_plan" "appserviceplan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "B1" # <-- Cambio sugerido: de F1 a B1
+  sku_name            = "F1" # <-- Cambio sugerido: de F1 a B1
 }
 
 # Create the Web App
